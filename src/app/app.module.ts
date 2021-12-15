d7a515fb06cf27d4153f4ff769b6abe71f606d04
gukropiwiec <gustavosk17@hotmail.com>
2021-12-14 23:26:33 -0300

    about section finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,18 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { SectionAboutComponent } from './components/section-about/section-about.component';
+import { SectionContactComponent } from './components/section-contact/section-contact.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SectionAboutComponent
+    SectionAboutComponent,
+    SectionContactComponent
   ],
   imports: [
     BrowserModule,
