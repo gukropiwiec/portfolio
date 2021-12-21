a8aa00c7bc5da66056f37846216601f3d99f6a06
gukropiwiec <gustavosk17@hotmail.com>
2021-12-21 14:53:35 -0300

    created tech interface

--- src/app/models/slide.interface.ts
+++ src/app/models/slide.interface.ts
@@ -1,9 +1,11 @@
+import { Tech } from "./tech.interface";
+
 export interface Slide {
     src: String;
     alt: String;
     title: String;
     href?: string;
     codeHref?: string;
-    techs: {name: string; fontColor: string;}[];
+    techs: Tech[];
     externalLinkTitle: string;
 }
