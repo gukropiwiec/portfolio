08d96dd52d9a3525ef2c8d99c6db963fda0f6860
gukropiwiec <gustavosk17@hotmail.com>
2022-10-02 01:15:32 -0300

    folder structure change and added some skills

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,43 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SectionAboutComponent } from './section-about/section-about.component';
+import { SectionContactComponent } from './section-contact/section-contact.component';
+import { SectionIntroComponent } from './section-intro/section-intro.component';
+import { SectionProjectsComponent } from './section-projects/section-projects.component';
+import { SectionSkillsComponent } from './section-skills/section-skills.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ProjectBoxComponent } from './project-box/project-box.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+const moduleComponents = [
+    CarouselComponent,
+    SectionAboutComponent,
+    SectionContactComponent,
+    SectionIntroComponent,
+    SectionProjectsComponent,
+    SectionSkillsComponent,
+    TimelineComponent,
+    ProjectBoxComponent,
+    NavbarComponent,
+    FooterComponent
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        TranslateModule,
+        FontAwesomeModule,
+        RouterModule,
+        CarouselModule
+    ],
+    declarations: moduleComponents,
+    exports: moduleComponents,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class ComponentsModule {}
